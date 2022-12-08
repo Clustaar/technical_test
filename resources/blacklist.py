aded41308b55e187f6c767f5f154e1416100d612
Axel schafers <axel.schafers@gmail.com>
2022-12-08 11:31:27 +0100

    French doc

--- resources/blacklist.py
+++ resources/blacklist.py
@@ -4,16 +4,21 @@ class BlackList:
         Il est autorisé de modifier le comportement de cette classe, sauf le nom des attributs
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
-        Initializes a set of blacklisted words
+        Initialise un set de mots à exclure.
         """
         self.bl = set()
         self.words = ""  # TODO load words.txt
         self._make_bl()
 
-    def _make_bl(self):
+    def _make_bl(self) -> None:
+        """
+        Strip les mots et les ajoute dans l'attribut bl
+        """
         words = self.words.split("\n")
+
         for w in words:
             w = w.strip()
+
             self.bl.add(w)
